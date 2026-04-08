49a01d82147d46f6238d4f73ce6d3b497f23cd73
sebasmaturanac <sebasmaturanac@gmail.com>
2026-04-08 13:45:11 -0300

    img wp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Explore a world of podcasts on Podcastio. Listen to engaging discussions, interviews, and more.">
-    <meta name="author" content="pixelaxis">
-    <meta name="robots" content="index, follow">
-    <meta name="keywords" content="podcast, audio, interviews, discussions, entertainment,ashadul">
+
     <title>@yield('title', 'Bohemio Ritual | Folklore, charlas y artistas en vivo')</title>
 
     <meta name="description" content="Bohemio Ritual es un canal de streaming con folklore, charlas, entrevistas y artistas en vivo. Un espacio para compartir música, historias y cultura argentina con identidad propia.">
@@ -26,24 +24,52 @@
     <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/img/fabicon.png') }}">
     <link rel="shortcut icon" href="{{ asset('assets/img/fabicon.png') }}" type="image/png">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/fabicon.png') }}">
-    <link rel="mask-icon" href="{{ asset('assets/img/fabicon.png') }}" color="#ffb340">
 
-    <!-- Open Graph / WhatsApp / Facebook -->
+    <!-- Open Graph / Facebook / WhatsApp -->
     <meta property="og:locale" content="es_AR">
     <meta property="og:type" content="website">
     <meta property="og:site_name" content="Bohemio Ritual">
     <meta property="og:title" content="@yield('og_title', 'Bohemio Ritual | Folklore, charlas y artistas en vivo')">
     <meta property="og:description" content="@yield('og_description', 'Bohemio Ritual es un canal de streaming con folklore, charlas, entrevistas y artistas en vivo. Música, cultura argentina e identidad propia en cada emisión.')">
     <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:image" content="{{ asset('assets/img/fabicon.png') }}">
-    <meta property="og:image:secure_url" content="{{ asset('assets/img/fabicon.png') }}">
+    <meta property="og:image" content="{{ secure_url('assets/img/bohemio-logo.png') }}">
+    <meta property="og:image:secure_url" content="{{ secure_url('assets/img/bohemio-logo.png') }}">
     <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="640">
     <meta property="og:image:alt" content="Bohemio Ritual">
 
-    <link rel="shortcut icon" href="{{ asset('assets/img/fabicon.png') }}" type="image/x-icon">
+    <!-- Twitter / X -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="@yield('og_title', 'Bohemio Ritual | Folklore, charlas y artistas en vivo')">
+    <meta name="twitter:description" content="@yield('og_description', 'Bohemio Ritual es un canal de streaming con folklore, charlas, entrevistas y artistas en vivo. Música, cultura argentina e identidad propia en cada emisión.')">
+    <meta name="twitter:image" content="{{ secure_url('assets/img/bohemio-logo.png') }}">
+    <meta name="twitter:image:alt" content="Bohemio Ritual">
+
+    <!-- Mobile -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Bohemio Ritual">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
+    <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+    <!-- Datos estructurados -->
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Bohemio Ritual",
+            "url": "{{ url('/') }}",
+            "logo": "{{ secure_url('assets/img/bohemio-logo.png') }}",
+            "sameAs": [
+                "https://www.youtube.com/@BohemioRitual"
+            ]
+        }
+    </script>
 </head>
 <body>
 
